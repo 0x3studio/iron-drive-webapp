215b9a7103c4a0fc109581e5f7f67f490d1fcdea
Vincent Battaglia <vincent.battaglia@gmail.com>
2023-03-05 08:59:43 +0100

    Tentative fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 const config: UserConfig = {
-  plugins: [sveltekit(), nodePolyfills()],
+  plugins: [sveltekit()],
   build: {
     target: "es2020",
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
   },
   optimizeDeps: {
-    esbuildOptions: { target: "es2020" },
+    esbuildOptions: {
+      target: "es2020",
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
 };
 
