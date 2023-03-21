2d8c98d3e86f538259ed4c334f60c18f6ad3ad33
Vincent Battaglia <vincent.battaglia@gmail.com>
2023-03-21 14:55:14 +0100

    Using API instead of Warp directly

--- src/lib/pages/File.svelte
+++ src/lib/pages/File.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-  import { WarpFactory } from "warp-contracts";
-  import { evmSignature } from "warp-contracts-plugin-signature";
-
   export let contractTxId: string;
   export let uuid: string;
 
+  const API_URL = import.meta.env.VITE_API_URL;
   const ARWEAVE_GATEWAY_URL = import.meta.env.VITE_ARWEAVE_GATEWAY_URL;
 
   const init = async () => {
-    const warp = WarpFactory.forMainnet({ inMemory: true, dbLocation: "2" });
+    const response = await fetch(`${API_URL}/${contractTxId}`);
+    const json = await response.json();
 
-    const contract = warp
-      .contract(contractTxId)
-      .connect({ type: "ethereum", signer: evmSignature });
+    const file = json.find((item: any) => item.id === uuid);
 
-    const { cachedValue } = await contract.readState();
-
-    if (cachedValue.state && cachedValue.state.files[uuid]) {
-      const txId = cachedValue.state.files[uuid].txId;
-      const url = `${ARWEAVE_GATEWAY_URL}/${txId}`;
+    if (file) {
+      const url = `${ARWEAVE_GATEWAY_URL}/${file.txId}`;
       window.location.href = url;
     } else {
       window.location.href = "/";
