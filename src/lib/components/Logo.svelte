935a3c854d3770da7b53cc0aa2be6755e02076d7
Lionel Cordier <lionel@ludus.one>
2023-03-22 14:34:55 +0100

    fix logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -6,12 +6,12 @@
 
   switch (size) {
     case "large": {
-      width = 168;
+      width = 158;
       height = 38;
       break;
     }
     default: {
-      width = 84;
+      width = 79;
       height = 19;
     }
   }
@@ -20,29 +20,33 @@
 <svg
   {width}
   {height}
-  viewBox="0 0 84 19"
+  viewBox="0 0 79 19"
   fill="currentColor"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
     fill-rule="evenodd"
     clip-rule="evenodd"
-    d="M25 19.0009H19L19 1.00095H25V7.19536V19.0009Z"
+    d="M22 19.0009H16L16 1.00095H22V7.19536V19.0009Z"
+    fill="black"
   />
   <path
-    d="M34 1.16548V8.00095H26C26.1948 6.68583 26.515 5.55728 26.9607 4.61529C27.5381 3.41059 28.3079 2.51098 29.2701 1.91646C30.6372 1.07184 32.4474 0.78682 34 1.16548Z"
+    d="M31 1.16548V8.00095H23C23.1948 6.68583 23.515 5.55728 23.9607 4.61529C24.5381 3.41059 25.3079 2.51098 26.2701 1.91646C27.6372 1.07184 29.4474 0.78682 31 1.16548Z"
+    fill="black"
   />
-  <circle cx="50" cy="10" r="9" />
-  <rect y="1" width="6" height="18" />
-  <path
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M67 0.929311V18.9293H83L83 5.9293C83 3.16788 80.7614 0.929307 78 0.929307L67 0.929311Z"
-  />
+  <circle cx="47" cy="10" r="9" fill="black" />
+  <rect y="1" width="6" height="18" fill="black" />
   <path
     fill-rule="evenodd"
     clip-rule="evenodd"
-    d="M67 1V19H72L72 5.99999C72 3.23857 69.7614 1 67 1Z"
+    d="M63 0.929311V18.9293H79L79 5.9293C79 3.16788 76.7614 0.929307 74 0.929307L63 0.929311Z"
+    fill="black"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M66 1V19H71L71 5.99999C71 3.23857 68.7614 1 66 1Z"
+    fill="black"
   />
 </svg>
 
