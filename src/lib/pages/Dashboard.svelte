d7d19a4d84586db83a4cf9e46e2159f5a531db13
Vincent Battaglia <vincent.battaglia@gmail.com>
2023-03-28 15:09:03 +0200

    Correct init

--- src/lib/pages/Dashboard.svelte
+++ src/lib/pages/Dashboard.svelte
@@ -195,6 +195,7 @@
   //   const { contractTxId: _contractTxId } = await warp.deploy({
   //     wallet: userSigner,
   //     initState: JSON.stringify({
+  //       users: {},
   //       owner: null,
   //       canEvolve: true,
   //     }),
