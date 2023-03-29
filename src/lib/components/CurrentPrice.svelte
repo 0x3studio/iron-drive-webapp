0d4977515dba032479d78be834123cec4b9c3c3b
Lionel Cordier <lionel@ludus.one>
2023-03-29 11:24:52 +0200

    update price in a store

--- src/lib/components/CurrentPrice.svelte
+++ src/lib/components/CurrentPrice.svelte
@@ -1,44 +1,18 @@
 <script lang="ts">
   import { bundlrStore, chainId, pricePerGb } from "$lib/stores";
   import { chainInfo } from "$lib/utils/chain";
-  import { getPrice } from "$lib/utils/price";
-  import { onDestroy } from "svelte";
-
-  let status: string = "working";
-  let price: number;
-  let priceUSD: number;
-
-  const _getPrice = async () => {
-    status = "working";
-
-    const data = await getPrice($bundlrStore, $chainId);
-    price = data.price;
-    priceUSD = data.priceUSD;
-    $pricePerGb.price = price;
-    $pricePerGb.priceUSD = priceUSD;
-    status = "done";
-  };
-
-  _getPrice();
-  const interval = setInterval(() => {
-    _getPrice();
-  }, 30000);
-
-  onDestroy(() => {
-    clearInterval(interval);
-  });
 </script>
 
 <div>
   <span>1 GB =</span>
-  {#if status === "working"}
+  {#if !$pricePerGb.priceUSD}
     <span>...</span>
   {:else}
     <span>
-      {#if priceUSD}
-        ${priceUSD.toFixed(2)}
+      {#if $pricePerGb.priceUSD}
+        ${$pricePerGb.priceUSD.toFixed(2)}
       {/if} |
-      {$bundlrStore.utils.unitConverter(price).toFixed(5)}
+      {$bundlrStore.utils.unitConverter($pricePerGb.price).toFixed(5)}
       {chainInfo($chainId).symbol}
     </span>
   {/if}
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,12 +1,43 @@
 import type { WebBundlr } from "@bundlr-network/client";
 
-import { writable, type Writable } from "svelte/store";
+import { get, writable, type Writable } from "svelte/store";
 import type BigNumber from "bignumber.js";
+import { getPrice } from "./utils/price";
+import type { Timeout } from "svooltip/types";
 
 export const bundlrStore: Writable<WebBundlr> = writable();
 export const balance: Writable<BigNumber> = writable();
 export const chainId: Writable<string> = writable();
-export const pricePerGb = writable({
-  price: 0,
-  priceUSD: 0,
-});
+
+function createPricePerGb() {
+  let id: null | Timeout = null;
+  const { subscribe, set, update } = writable({ price: 0, priceUSD: 0 });
+  console.log("SUBSCRIBE", subscribe);
+  bundlrStore.subscribe((bundlr) => {
+    if (bundlr) {
+      const updatePrice = async () => {
+        if (bundlr) {
+          const _chainId = get(chainId);
+          const data = await getPrice(bundlr, _chainId);
+          const price = data.price;
+          const priceUSD = data.priceUSD;
+          console.log("UPDATE PRICE", bundlr, _chainId, price, priceUSD);
+          update(() => ({ price, priceUSD }));
+        }
+      };
+      updatePrice();
+      if (id) clearInterval(id);
+      id = setInterval(() => {
+        updatePrice();
+      }, 30000);
+    }
+  });
+
+  return {
+    set,
+    update,
+    subscribe,
+  };
+}
+
+export const pricePerGb = createPricePerGb();
